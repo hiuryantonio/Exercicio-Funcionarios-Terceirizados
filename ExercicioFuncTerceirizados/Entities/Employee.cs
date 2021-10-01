b17cdbd4da125650235617d798dbd57e965ba80c
Hiury Gomes <hiury_jp@hotmail.com>
2021-10-01 16:47:34 -0300

    Exercicio Funcionario terceirizado

---
+++ ExercicioFuncTerceirizados/Entities/Employee.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace ExercicioFuncTerceirizados.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(String name, Int hours, Double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
---
+++ ExercicioFuncTerceirizados/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ExercicioFuncTerceirizados.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+        public OutsourcedEmployee() 
+        {
+        }
+
+        public OutsourcedEmployee(String name, Int hours, Double valuePerHour, Double additionalCharge)
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override Double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
